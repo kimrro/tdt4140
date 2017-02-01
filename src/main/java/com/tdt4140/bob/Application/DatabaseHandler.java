cd59ab469399b760aebbff5e464c96ceaec3b211
KimRobin <KimRobin@10.22.44.245>
2017-02-01 15:34:43 +0100

    Skeleton

---
+++ src/main/java/com/tdt4140/bob/Application/DatabaseHandler.java
@@ -0,0 +1,44 @@
+package com.tdt4140.bob.Application;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class DatabaseHandler {
+
+	private Connection con = null;
+	
+	private final String url = "jdbc:mysql://west2-mysql-bob.c9ztinmq6h0z.us-west-2.rds.amazonaws.com:3306/dbbob?autoConnect=true&useSSL=false";
+	private final String username = "kimrro";
+	private final String password = "dbbob123";
+	
+	public void connect() {
+		System.out.println("Connecting to database...");
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			con = DriverManager.getConnection(url, username, password);
+			if (this.con != null) {
+				System.out.println("Connected!");
+			}
+		} catch (SQLException e) {
+			System.out.println("Could not connect to database\nERROR: " + e.getSQLState());
+		} catch (ClassNotFoundException e) {
+			System.out.println("Could not find driver\nERROR: " + e.getMessage());
+		}
+		
+	}
+	
+	public void close() {
+		try {
+			con.close();
+		} catch (SQLException e) {
+			System.out.println("Could not close connection to database\nERROR: " + e.getSQLState());
+		}
+	}
+
+	public PreparedStatement prepareQuery(String query) throws SQLException {
+		return con.prepareStatement(query);
+	}
+
+}
---
+++ src/main/java/com/tdt4140/bob/Application/Main.java
@@ -0,0 +1,9 @@
+package com.tdt4140.bob.Application;
+
+public class Main {
+
+	public static void main(String[] args) {
+		DatabaseHandler dbh = new DatabaseHandler();
+		dbh.connect();
+	}
+}
