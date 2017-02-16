e3da7d958e452a2f718d1aed7f4d71a42e5ad2a3
KimRobin <KimRobin@10.22.44.184>
2017-02-16 14:11:10 +0100

    Minor fix to show all users registered

--- src/main/java/com/tdt4140/bob/Application/Example/ExampleInfo.java
+++ src/main/java/com/tdt4140/bob/Application/Example/ExampleInfo.java
@@ -8,14 +8,16 @@ import com.tdt4140.bob.Application.DatabaseHandler;
 public class ExampleInfo {
 	public String getUserInfo(DatabaseHandler dbh) throws SQLException {
 		ResultSet rs = ExampleHandler.getAllUserInfo(dbh);
-		if(rs.next()) {
+		String allUsers = "";
+		while (rs.next()) {
 			String user = rs.getString("username");
 			String password = rs.getString("password");
 			String fname = rs.getString("fname");
 			String lname = rs.getString("lname");
-			return ("Username: " + user + "\nPassword: " + password + "\nFornavn: " + fname + "\nEtternavn: " + lname);
-		} else {
-			return ("No user information found.");
+			allUsers += ("\n\nUsername: " + user + "\nPassword: " + password + "\nFornavn: " + fname + "\nEtternavn: " + lname);
 		}
+		
+		return allUsers;
 	}
+
 }
