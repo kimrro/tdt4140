b96d481f91200d6b97ffe88bdcacc4e96ef3802e
Burg <Burg@jrgens-mbp-2.lan>
2017-02-20 18:48:12 +0100

    Test3

--- src/main/java/com/tdt4140/bob/Application/Login/LoginHandler.java
+++ src/main/java/com/tdt4140/bob/Application/Login/LoginHandler.java
@@ -4,28 +4,28 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-import com.tdt4140.bob.Application.BCrypt;
 import com.tdt4140.bob.Application.DatabaseHandler;
-import com.tdt4140.bob.JavaFX.*;
 
 public class LoginHandler {
-	
-	public static Login login(String userName, String password, DatabaseHandler dbh) throws SQLException {
-        ResultSet userData = getUserPassword(userName, dbh);
-        //check if the password matches the encrypted one
-        if (userData.next()) {
-            if (BCrypt.checkpw(password, userData.getString(4))) {
-                return null; //return new User(userData);
-            }
-        }
-        return null;
-    }
-	
-	public static ResultSet getUserPassword(String username, DatabaseHandler dbh) throws SQLException {
-		String query = "SELECT password FROM user " +
-					   "WHERE username = ?";
+
+	public ResultSet getUserCredentials(DatabaseHandler dbh, String username) throws SQLException {
+		String query = "SELECT * FROM user WHERE username = ?";
 		PreparedStatement prepStatement = dbh.prepareQuery(query);
 		prepStatement.setString(1, username);
 		return prepStatement.executeQuery();
 	}
+
+	public void registerUser(DatabaseHandler dbh, String username, String password, String fname, String lname) throws SQLException {
+		String query = "INSERT INTO user (username, password, fname, lname) " +
+					   "VALUES (?, ?, ?, ?)";
+		PreparedStatement prepStatement = dbh.prepareQuery(query);
+		
+		prepStatement.setString(1, username);
+		prepStatement.setString(2, password);
+		prepStatement.setString(3, fname);
+		prepStatement.setString(4, lname);
+		
+		prepStatement.executeUpdate();
+	}
+
 }
--- src/main/java/com/tdt4140/bob/JavaFX/Controllers/LoginController.java
+++ src/main/java/com/tdt4140/bob/JavaFX/Controllers/LoginController.java
@@ -6,6 +6,7 @@ import java.sql.SQLException;
 import com.tdt4140.bob.Application.DatabaseHandler;
 import com.tdt4140.bob.Application.Login.LoginHandler;
 
+import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.paint.Paint;
 import javafx.scene.text.Text;
@@ -15,33 +16,36 @@ public class LoginController extends Controller{
 	private TextField username;
 	private PasswordField password;
 	private Text actionTarget;
+	private Button button;
 	
-	protected void buttonClicked() {
-		DatabaseHandler dbh = new DatabaseHandler("com.mysql.jdbc.Driver", 
-				"jdbc:mysql://rds-mysql-bob.c9ztinmq6h0z.us-west-2.rds.amazonaws.com:3306/bobdb?autoConnect=true&useSSL=false",
-				"robert",
-				"dbbob123");
-		app.getDatabaseHandler();
-
-        try {
-            LoginHandler.login(username.getText(), password.getText(), dbh);
-        } catch (SQLException e) {
+	private DatabaseHandler dbh;
+	private LoginHandler lh;
+	String name, pw;
+	
+	public void buttonClicked() throws SQLException{
+		dbh = app.getDatabaseHandler();
+		
+		name = username.getText();
+		pw = password.getText();
+		
+		try {
+			lh.getUserCredentials(dbh, name).next();
+		} catch (SQLException e) {
             System.out.println(e.getMessage());
             actionTarget.setFill(Paint.valueOf("REDBRICK"));
             actionTarget.setText("404_FeilOppdaget_Ring_Lars: 45245345");
-        }
-        if (username.getText() != null) {
-            actionTarget.setText("Velkommen");
-            try {
-                app.makeDash();
-            } catch (Exception e) {
-                System.out.println(e.getMessage());
-            }
-        }
-        else {
+            
+        } if (lh.getUserCredentials(dbh, name).findColumn("password") == pw) {
+			 actionTarget.setFill(Paint.valueOf("GREEN"));
+				actionTarget.setText("Velkommen");
+				try {
+	                app.makeDash();
+	            } catch (Exception e) {
+	                System.out.println(e.getMessage());
+	            }
+		} else {
             actionTarget.setFill(Paint.valueOf("#ff3636"));
             actionTarget.setText("Feil brukernavn eller passord");
         }
-    
     }
 }
