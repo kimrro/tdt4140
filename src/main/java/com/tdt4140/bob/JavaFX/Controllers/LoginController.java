b6d61548aadd2c0be171f26156a4d84f23794697
KimRobin <KimRobin@10.22.44.160>
2017-02-21 11:09:01 +0100

    Login fix

--- src/main/java/com/tdt4140/bob/JavaFX/Controllers/LoginController.java
+++ src/main/java/com/tdt4140/bob/JavaFX/Controllers/LoginController.java
@@ -1,59 +1,21 @@
 package com.tdt4140.bob.JavaFX.Controllers;
 
-import java.awt.TextField;
-import java.sql.SQLException;
-
-import com.tdt4140.bob.Application.DatabaseHandler;
-import com.tdt4140.bob.Application.Login.LoginHandler;
-
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
-import javafx.scene.paint.Paint;
-import javafx.scene.text.Text;
+import javafx.scene.control.TextField;
 
 public class LoginController extends Controller{
 	
 	@FXML
-	private TextField username;
+	private TextField txtUsername;
 	@FXML
-	private PasswordField password;
+	private PasswordField passPassword;
 	@FXML
-	private Text actionTarget;
-	@FXML
-	private Button button;
+	private Button btnLogin;
 	
-	private DatabaseHandler dbh;
-	private LoginHandler lh;
-	String name; 
-	String pwd;
+	public void onLogin() {
+		System.out.println(txtUsername.getText());
+	}
 	
-	public void buttonClicked() throws SQLException{
-		dbh = app.getDatabaseHandler();
-		
-		name = username.getText();
-		pwd = password.getText();
-		
-		actionTarget.setText("Test");
-		
-		/*try {
-			lh.getUserCredentials(dbh, name).next();
-		} catch (SQLException e) {
-            System.out.println(e.getMessage());
-            actionTarget.setFill(Paint.valueOf("REDBRICK"));
-            actionTarget.setText("404_FeilOppdaget_Ring_Lars: 45245345");
-            
-        } if (lh.getUserCredentials(dbh, name).findColumn("password") == pwd) {
-			 actionTarget.setFill(Paint.valueOf("GREEN"));
-				actionTarget.setText("Velkommen");
-				try {
-	                app.makeDash();
-	            } catch (Exception e) {
-	                System.out.println(e.getMessage());
-	            }
-		} else {
-            actionTarget.setFill(Paint.valueOf("#ff3636"));
-            actionTarget.setText("Feil brukernavn eller passord");
-        } */
-    }
 }
