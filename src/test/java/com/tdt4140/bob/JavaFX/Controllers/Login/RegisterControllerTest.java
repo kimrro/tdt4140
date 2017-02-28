94824f985c0a94b35399604fe115b3d1ee625138
KimRobin <KimRobin@10.22.46.35>
2017-02-28 14:43:11 +0100

    Finished (again)

--- src/test/java/com/tdt4140/bob/JavaFX/Controllers/Login/RegisterControllerTest.java
+++ src/test/java/com/tdt4140/bob/JavaFX/Controllers/Login/RegisterControllerTest.java
@@ -1,7 +1,26 @@
 package com.tdt4140.bob.JavaFX.Controllers.Login;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import com.tdt4140.bob.Application.DatabaseHandler;
+import com.tdt4140.bob.Application.Login.LoginHandler;
+import com.tdt4140.bob.JavaFX.Bob;
+
 public class RegisterControllerTest extends junit.framework.TestCase {
 	RegisterController rc = new RegisterController();
+	
+	public void testExisitingUsername() throws SQLException {
+		Bob bob = new Bob();
+		DatabaseHandler dbh = bob.getDatabaseHandler();
+		LoginHandler lh = new LoginHandler();
+		
+		ResultSet test1 = lh.getUserCredentials(dbh, "test");
+		ResultSet test2 = lh.getUserCredentials(dbh, "eplekake");
+		
+		assertTrue(test1.next());
+		assertFalse(test2.next());
+	}
 	
 	public void testValidUsername() {
 		assertTrue(rc.isValidUsername("test!#%"));
