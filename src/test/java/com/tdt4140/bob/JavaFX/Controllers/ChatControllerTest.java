c1eae5a8f0e49f150f527b03de9dc3dedbbb01a8
Lars-August <Lars-August@dhcp-10-22-33-156.wlan.ntnu.no>
2017-02-28 14:02:27 +0100

    Begynnelsen på chattest

---
+++ src/test/java/com/tdt4140/bob/JavaFX/Controllers/ChatControllerTest.java
@@ -0,0 +1,15 @@
+package com.tdt4140.bob.JavaFX.Controllers;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import com.tdt4140.bob.JavaFX.Controllers.*;
+
+public class ChatControllerTest extends junit.framework.TestCase{
+	ChatController CC= new ChatController();
+	@Test
+	public void testChatClicked() {
+		assertEquals("",botSay);
+	}
+
+}
