2dafae8272614458596619f6bc01843d1e589189
Burg <Burg@dhcp-10-22-38-169.wlan.ntnu.no>
2017-03-28 14:22:49 +0200

    Keywords now up to date!!!!

--- src/main/java/com/tdt4140/bob/Application/Chat/ChatHandler.java
+++ src/main/java/com/tdt4140/bob/Application/Chat/ChatHandler.java
@@ -23,5 +23,57 @@ public class ChatHandler {
 		prepStatement.setString(1, coursename);
 		return prepStatement.executeQuery();
 	}
+	
+	public static ResultSet getSubjectCode(DatabaseHandler dbh, String coursename) throws SQLException {
+		String query = "SELECT code FROM subject WHERE coursename = ?";
+		PreparedStatement prepStatement = dbh.prepareQuery(query);
+		prepStatement.setString(1, coursename);
+		return prepStatement.executeQuery();
+	}
+	
+//	public static ResultSet getFreq(DatabaseHandler dbh, String keyword) throws SQLException {
+//		String query = "SELECT frequency FROM search WHERE keyword = ?";
+//		PreparedStatement prepStatement = dbh.prepareQuery(query);
+//		prepStatement.setString(1, keyword);
+//		return prepStatement.executeQuery();
+//	}
+	
+	public static int writeKeywords(DatabaseHandler dbh, String keyword, String code) throws SQLException {
+		int count = 0;
+		int freq = 0;
+		final String queryCheck = "SELECT count(*) FROM search WHERE keyword = ? AND code = ?";
+		final PreparedStatement prepStatement = dbh.prepareQuery(queryCheck);
+		prepStatement.setString(1, keyword);
+		prepStatement.setString(2, code);
+		final ResultSet resultSet = prepStatement.executeQuery();
+		if(resultSet.next()) {
+		    count = resultSet.getInt(1);
+		}
+		
+		if (count == 0) {
+			String query = "INSERT INTO search (keyword, frequency, code) VALUES(?, ?, ?)";
+			PreparedStatement prepStatement1 = dbh.prepareQuery(query);
+			prepStatement1.setString(1, keyword);
+			prepStatement1.setInt(2, 1);
+			prepStatement1.setString(3, code);
+			return prepStatement1.executeUpdate();
+		} else {
+//			ResultSet rs = getFreq(dbh, keyword);
+//			if (rs.next()) {
+//				freq = rs.getInt("frequency");
+//			}
+			
+			String query = "UPDATE search SET frequency = frequency + 1 WHERE keyword = ? AND code = ?";
+			
+			PreparedStatement prepStatement1 = dbh.prepareQuery(query);
+			prepStatement1.setString(1, keyword);
+			prepStatement1.setString(2, code);
+			return prepStatement1.executeUpdate();
+		}
+		
+		
+	}
+	
+	
 
 }
--- src/main/java/com/tdt4140/bob/JavaFX/Controllers/ChatController.java
+++ src/main/java/com/tdt4140/bob/JavaFX/Controllers/ChatController.java
@@ -54,6 +54,7 @@ public class ChatController extends Controller {
 	private DatabaseHandler dbh;
 	private ArrayList<String> subjects = new ArrayList<String>();
 	private String pick;
+	private ResultSet rs = null;
 	
 	public void onLoad() {
 		pick = "";
@@ -95,160 +96,174 @@ public class ChatController extends Controller {
 	}
 	
 	public void chatClicked() throws IOException, SQLException {
-			String replie = "ERROR";
-			String uText = input.getText();
-			
-			String TEXT = uText.toLowerCase();
-			String part[] = TEXT.split(" ");
-			int n = part.length;
-			
-			if (!(pick.equals(""))) {
-				if ((TEXT.contains("hello")) || (TEXT.contains("hi"))){
-					int reply_decider = (int) (Math.random() * 3 + 1);
-					if (reply_decider == 1) {
-							replie = "Hello there!";
-					}
-					else if (reply_decider == 2) {
-						replie = "How you doin? ^^";
-					}
-					else if (reply_decider == 3) {
-						replie = "Hello mate!";
-					}
+		String replie = "ERROR";
+		String uText = input.getText();
+		
+		String TEXT = uText.toLowerCase();
+		String part[] = TEXT.split(" ");
+		int n = part.length;
+		
+		if (!(pick.equals(""))) {
+			if ((TEXT.contains("hello")) || (TEXT.contains("hi"))){
+				int reply_decider = (int) (Math.random() * 3 + 1);
+				if (reply_decider == 1) {
+						replie = "Hello there!";
 				}
-				
-				else if ((part[0].equals("what") || part[0].equals("who") || part[0].equals("where")) && part[1].contains("is")) {
-					ArrayList<String> list = new ArrayList<String>();
-					
-					String subject = "";
-					for (int i = 2; i < n; i++) {
-						list.add(part[i]);
-					}
-					
-					for (String s : list) {
-					    subject += s + " ";
-					}
-					
-					String input1 = subject;
-					String input2 = WordUtils.capitalizeFully(subject);
-					
-					if(wikipedia(input1).equals("0")) {
-						if (wikipedia(input2).equals("0")){
-							replie = "Please rephrase! I couldnt find what you where looking for :(";
-						} else {
-							replie = "I found this on wikipedia: " + input2 + "\n" + wikipedia(input2);
-						}
-					} else {
-						replie = "I found this on wikipedia: " + input2 + "\n" + wikipedia(input1);
-					}
+				else if (reply_decider == 2) {
+					replie = "How you doin? ^^";
 				}
-				
-				else if (TEXT.equals("/subjects")){
-					int reply_decider = (int) (Math.random()* 2 + 1);
-					if (reply_decider== 1) {
-						replie = "You have the subjects: \n" + subjects;
-					}
-					else if (reply_decider == 2) {
-						replie = "Currently, you are attending these classes: \n" + subjects;
-					}
+				else if (reply_decider == 3) {
+					replie = "Hello mate!";
 				}
-			
-				else if (part[0].equals("/lecturer")) {
-					ResultSet output = ChatHandler.getLecturer(dbh, pick);
-					ArrayList<String> lecturer = new ArrayList<String>();
-					while (output.next()) {
-						lecturer.add(output.getString("fornavn") + " " + output.getString("etternavn"));
-					}
-					if (lecturer.isEmpty()) {
-						replie = "I dont think there are registered any lecturers in that course..";
-					} else {
-						replie = "In " + pick + " teaches the lecturer: \n" + lecturer;
-					}
-				}
-				
-				else if ((part[0].equals("/clear"))) {
-					int reply_decider = (int) (Math.random() * 2 + 1);
-					if(reply_decider == 1){
-						replie = "Cleared!";
-					}
-					else if(reply_decider == 2){
-						replie = "Bye bye text =)";
-					}
-				}
-				
-				else if ((part[0].contains("/help")) || part[0].contains("/commands")) {
-						replie = "Possible commands are: \n \n" 
-								+ "1: /subjects \n" 
-								+ "2: /lecturer \n" 
-								+ "3: /clear \n" 
-								+ "4: Ask me something";
-				}
-				
-				else if (part[0].contains("h") && part[0].contains("e") && part[0].contains("l") && part[0].contains("p") && part[0] != "help") {
-					replie = "Did you mean '/help'?";
-				}
-				
-				else if (part[0].contains("c") && part[0].contains("o") && part[0].contains("m") && part[0].contains("n") && part[0] != "commands") {
-					replie = "Did you mean '/commands'?";
-				}
-				
-				else if (part[0].contains("c") && part[0].contains("l") && part[0].contains("e") && part[0].contains("a") && part[0].contains("r") && part[0] != "clear") {
-					replie = "Did you mean '/clear'?";
-				}
-				
-				else if (part[0].contains("w") && part[0].contains("h") && part[0].contains("a") && part[0] != "what") {
-					replie = "Did you mean to ask me a question?";
-				}
-
-				else if (TEXT.contains("i am") || (part[0].contains("i") && part[0].contains("m"))) {
-					int reply_decider = (int) (Math.random() * 2 + 1);
-					if (reply_decider == 1) {
-							replie = "Good for you!!";
-					}
-					else if (reply_decider == 2) {
-						replie = "Nice to hear that!";
-					}
-				}
-				
-				else if (TEXT.contains("how are you") || ((part[0].contains("how")) && (part[1].contains("are")) && (part[2].contains("you")))) {
-					int reply_decider = (int) (Math.random() * 3 + 1);
-					if (reply_decider == 1) {
-						replie = "I'm doing well thanks! And you?";
-					}
-					else if (reply_decider == 2){
-						replie = "Not too bad, and for you?";
-					}
-					else if(reply_decider == 3) {
-						replie = "Very well thank you! How is it going for you?";
-					}
-				}
-			
-				else {
-					int reply_decider = (int) (Math.random() * 4 + 1);
-					if(reply_decider == 1) {
-						replie = "I didn't get that, try '/help' to show commands";
-					}
-					else if(reply_decider == 2) {
-						replie = "Please rephrase that or type '/commands' to show commands";
-					}
-					else if(reply_decider == 3) {
-						replie = "Type '/help' to show commands";
-					}
-					else if (reply_decider == 4) {
-						replie = "'/help' will show you some usefull comands =D";
-					}
-				}
-			
-				if (TEXT.contains("/clear")) {
-						input.setText("");
-						area.setText("");
-						chatArea(uText, replie);
-				} else {
-						input.setText("");
-						chatArea(uText, replie);
-				}
-			} else {
-				area.appendText("BOB: Please choose a subject! \n\n" );
 			}
+			
+			else if ((part[0].equals("what") || part[0].equals("who") || part[0].equals("where")) && part[1].contains("is")) {
+				ArrayList<String> list = new ArrayList<String>();
+				
+				String subject = "";
+				for (int i = 2; i < n; i++) {
+					list.add(part[i]);
+				}
+				
+				for (String s : list) {
+				    subject += s + " ";
+				}
+				
+				String input1 = subject;
+				String input2 = WordUtils.capitalizeFully(subject);
+				String code = null;
+				
+				if(wikipedia(input1).equals("0")) {
+					if (wikipedia(input2).equals("0")){
+						replie = "Please rephrase! I couldnt find what you where looking for :(";
+					} else {
+						replie = "I found this on wikipedia: " + input2 + "\n" + wikipedia(input2);
+						rs = ChatHandler.getSubjectCode(dbh, pick);
+						if (rs.next()) {
+							code = rs.getString("code");
+						}
+						
+						ChatHandler.writeKeywords(dbh, subject, code);
+						
+					}
+				} else {
+					replie = "I found this on wikipedia: " + input2 + "\n" + wikipedia(input1);
+					rs = ChatHandler.getSubjectCode(dbh, pick);
+					if (rs.next()) {
+						code = rs.getString("code");
+					}
+					
+					ChatHandler.writeKeywords(dbh, subject, code);
+				}
+			}
+			
+			else if (TEXT.equals("/subjects")){
+				int reply_decider = (int) (Math.random()* 2 + 1);
+				if (reply_decider== 1) {
+					replie = "You have the subjects: \n" + subjects;
+				}
+				else if (reply_decider == 2) {
+					replie = "Currently, you are attending these classes: \n" + subjects;
+				}
+			}
+		
+			else if (part[0].equals("/lecturer")) {
+				ResultSet output = ChatHandler.getLecturer(dbh, pick);
+				ArrayList<String> lecturer = new ArrayList<String>();
+				while (output.next()) {
+					lecturer.add(output.getString("fornavn") + " " + output.getString("etternavn"));
+				}
+				if (lecturer.isEmpty()) {
+					replie = "I dont think there are registered any lecturers in that course..";
+				} else {
+					replie = "In " + pick + " teaches the lecturer: \n" + lecturer;
+				}
+			}
+			
+			else if ((part[0].equals("/clear"))) {
+				int reply_decider = (int) (Math.random() * 2 + 1);
+				if(reply_decider == 1){
+					replie = "Cleared!";
+				}
+				else if(reply_decider == 2){
+					replie = "Bye bye text =)";
+				}
+			}
+			
+			else if ((part[0].equals("/help")) || part[0].equals("/commands")) {
+					replie = "Possible commands are: \n \n" 
+							+ "1: /subjects \n" 
+							+ "2: /lecturer \n" 
+							+ "3: /clear \n" 
+							+ "4: Ask me something";
+			}
+			
+			else if (part[0].contains("h") && part[0].contains("e") && part[0].contains("l") && part[0].contains("p") && part[0] != "help") {
+				replie = "Did you mean '/help'?";
+			}
+			
+			else if (part[0].contains("c") && part[0].contains("o") && part[0].contains("m") && part[0].contains("n") && part[0] != "commands") {
+				replie = "Did you mean '/commands'?";
+			}
+			
+			else if (part[0].contains("c") && part[0].contains("l") && part[0].contains("e") && part[0].contains("a") && part[0].contains("r") && part[0] != "clear") {
+				replie = "Did you mean '/clear'?";
+			}
+			
+			else if (part[0].contains("w") && part[0].contains("h") && part[0].contains("a") && part[0] != "what") {
+				replie = "Did you mean to ask me a question?";
+			}
+
+			else if (TEXT.contains("i am") || (part[0].contains("i") && part[0].contains("m"))) {
+				int reply_decider = (int) (Math.random() * 2 + 1);
+				if (reply_decider == 1) {
+						replie = "Good for you!!";
+				}
+				else if (reply_decider == 2) {
+					replie = "Nice to hear that!";
+				}
+			}
+			
+			else if (TEXT.contains("how are you") || ((part[0].contains("how")) && (part[1].contains("are")) && (part[2].contains("you")))) {
+				int reply_decider = (int) (Math.random() * 3 + 1);
+				if (reply_decider == 1) {
+					replie = "I'm doing well thanks! And you?";
+				}
+				else if (reply_decider == 2){
+					replie = "Not too bad, and for you?";
+				}
+				else if(reply_decider == 3) {
+					replie = "Very well thank you! How is it going for you?";
+				}
+			}
+		
+			else {
+				int reply_decider = (int) (Math.random() * 4 + 1);
+				if(reply_decider == 1) {
+					replie = "I didn't get that, try '/help' to show commands";
+				}
+				else if(reply_decider == 2) {
+					replie = "Please rephrase that or type '/commands' to show commands";
+				}
+				else if(reply_decider == 3) {
+					replie = "Type '/help' to show commands";
+				}
+				else if (reply_decider == 4) {
+					replie = "'/help' will show you some usefull comands =D";
+				}
+			}
+		
+			if (TEXT.contains("/clear")) {
+					input.setText("");
+					area.setText("");
+					chatArea(uText, replie);
+			} else {
+					input.setText("");
+					chatArea(uText, replie);
+			}
+		} else {
+			area.appendText("BOB: Please choose a subject! \n\n" );
+		}
 
 			
 	}
