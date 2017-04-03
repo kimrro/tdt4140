627ff189a122d710b522561b1e4238c6434a3e48
Lars-August <Lars-August@dhcp-10-22-33-204.wlan.ntnu.no>
2017-04-03 15:23:11 +0200

    forgot to add the last change.

---
+++ src/main/java/com/tdt4140/bob/Application/Curriculum/CurriculumHandler.java
@@ -0,0 +1,19 @@
+package com.tdt4140.bob.Application.Curriculum;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import com.tdt4140.bob.Application.DatabaseHandler;
+
+public class CurriculumHandler {
+	
+	public  ResultSet addKeyword(DatabaseHandler dbh, String page, String keyword, String code) throws SQLException {
+		String query = "INSERT INTO curriculum VALUES(?,?,?)";
+		PreparedStatement prepStatement = dbh.prepareQuery(query);
+		prepStatement.setString(1, code);
+		prepStatement.setString(2, keyword);
+		prepStatement.setString(3, page);
+		return prepStatement.executeQuery();
+	}
+}
